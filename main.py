048a0a166fae0c022839bdbeccfe9347268df9ca
cecinuga <cecinuga.tdb@gmail.com>
2026-07-04 21:08:26 +0200

    updates

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from core.activations import GELU
 from core.tensor import Tensor
+from core.losses import log_softmax
 
 def main():
-    a = Tensor([[[1,2,3], [4,5,6], [7, 8, 9], [10, 11, 12]], [[13,14,15], [16,17,18], [19, 20, 21], [22, 23, 24]]])
-    gelu = GELU()
-
-    print(a.shape)
-    print(gelu(a))
-    print()
+    a = Tensor([[1,2,3], [4,5,6]])
+    print(log_softmax(a))
 
 
 if __name__ == "__main__":

490e20d67dd5686d3d9887ef1a2d494909c6f1a2
cecinuga <cecinuga.tdb@gmail.com>
2026-07-06 23:58:30 +0200

    creating regression examples

--- examples/regression.py
+++ examples/regression.py
@@ -1,10 +1,18 @@
-# %% Setup
 import numpy as np
 import matplotlib.pyplot as plt
-from core.losses import log_softmax
-from core.tensor import Tensor
 
-# %% Forward pass
-tensor = Tensor([1, 2, 3])
-res = log_softmax(tensor)
-print(res)
+def rescale(x: np.ndarray, n: float):
+    """Rescale the (0, 1) range in (-n, n)"""
+    return n*(2*x - 1)
+
+def f(x: np.ndarray):
+    """The function we want to estimate"""
+    return x*2 + 5
+
+domain = rescale(np.random.random(size=20), 100) # Equivalent of np.linspace(-10, 10, 100)
+y = f(domain)
+
+noisy_y = y + np.random.rand(*y.shape)*30
+
+plt.plot(domain, noisy_y, 'r.')
+plt.show()

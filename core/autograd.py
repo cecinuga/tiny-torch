123f4569f363d0b3e9cfd65dcf5900819a9e50b7
cecinuga <cecinuga.tdb@gmail.com>
2026-07-08 22:38:23 +0200

    implementing autograd engine

--- core/autograd.py
+++ core/autograd.py
@@ -1,5 +1,5 @@
-from typing import override
 import numpy as np
+from typing import override
 from core.tensor import Tensor
 
 def enable_autograd(quiet:bool=False) -> None:
@@ -10,7 +10,7 @@ class Function:
         # The Memory Cost
         self.saved_tensors:tuple[Tensor, ...] = tensors
         # The Graph Structure
-        self.next_functions:list[Function] = []
+        self.next_functions:list[Function|None] = [t._grad_fn for t in tensors]
 
     def apply(self, grad_output: np.ndarray)-> tuple[np.ndarray, ...]:
         """Compute gradients for the inputs."""
@@ -76,10 +76,12 @@ class MatmulBackward(Function):
 
         # Aligns shapes by transposing the partner matrix
         if a.requires_grad:
-            grad_a = np.matmul(grad_output, b.data.T)
+            b_T = np.swapaxes(b.data, -2, -1)
+            grad_a = np.matmul(grad_output, b_T)
 
         if b.requires_grad:
-            grad_b = np.matmul(a.data.T, grad_output)
+            a_T = np.swapaxes(a.data, -2, -1)
+            grad_b = np.matmul(a_T, grad_output)
 
         return grad_a, grad_b
 

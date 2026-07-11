fc3c06fc53496a5bd704a7302f5c57ad74ab1f71
cecinuga <cecinuga.tdb@gmail.com>
2026-07-11 15:08:53 +0200

    implemented backward dag computation

--- core/activations.py
+++ core/activations.py
@@ -1,3 +1,4 @@
+from core.autograd import ReLUBackward, SigmoidBackward, TanhBackward, GELUBackward, SoftmaxBackward
 from core.functions import relu, sigmoid, tanh, gelu, softmax
 from typing import override
 from core.layer import Layer
@@ -6,30 +7,34 @@ from core.tensor import Tensor
 class ReLU(Layer):
     @override
     def forward(self, x: Tensor) -> Tensor:
-        """Apply ReLU: max(0, x)"""
-        res = relu(x.data)
-        return Tensor(res)
+        res = Tensor(relu(x.data))
+        res._grad_fn = ReLUBackward(x)
+        return res
 
 class Sigmoid(Layer):
     @override
     def forward(self, x: Tensor) -> Tensor:
-        res = sigmoid(x.data)
+        res = Tensor(sigmoid(x.data))
+        res._grad_fn = SigmoidBackward(x)
         return Tensor(res)
 
 class Tanh(Layer):
     @override
     def forward(self, x: Tensor) -> Tensor:
-        res = tanh(x.data)
-        return Tensor(res)
+        res = Tensor(tanh(x.data))
+        res._grad_fn = TanhBackward(x)
+        return res
 
 class GELU(Layer):
     @override
     def forward(self, x: Tensor) -> Tensor:
-        res = gelu(x.data)
-        return Tensor(res)
+        res = Tensor(gelu(x.data))
+        res._grad_fn = GELUBackward(x)
+        return res
 
 class Softmax(Layer):
     @override
     def forward(self, x: Tensor, dim: int = -1) -> Tensor:
-        res = softmax(x.data, dim)
-        return Tensor(res)
+        res = Tensor(softmax(x.data, dim))
+        res._grad_fn = SoftmaxBackward(x)
+        return res

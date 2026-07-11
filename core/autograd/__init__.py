3449708c6b9c164eb583d55f6ca5655bdae82fa8
cecinuga <cecinuga.tdb@gmail.com>
2026-07-11 15:45:12 +0200

    exported DivBackward

--- core/autograd/__init__.py
+++ core/autograd/__init__.py
@@ -1,12 +1,13 @@
 from core.autograd.base import Function
 from core.autograd.activations import ReLUBackward, SigmoidBackward, TanhBackward, GELUBackward, SoftmaxBackward
-from core.autograd.arithmetic import AddBackward, SubBackward, MulBackward, MatmulBackward, ReshapeBackward, TransposeBackward
+from core.autograd.arithmetic import AddBackward, SubBackward, MulBackward, DivBackward, MatmulBackward, ReshapeBackward, TransposeBackward
 
 __all__ = [
     "Function",
     "AddBackward",
     "SubBackward",
     "MulBackward",
+    "DivBackward",
     "MatmulBackward",
     "ReshapeBackward",
     "TransposeBackward",

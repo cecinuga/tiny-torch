39b9fd197e56336a5479d6f684688d860595cf03
cecinuga <cecinuga.tdb@gmail.com>
2026-07-11 18:35:56 +0200

    wired losses backward fun

--- core/losses.py
+++ core/losses.py
@@ -1,27 +1,32 @@
+from core.autograd.losses import CrossEntropyBackward, MSELossBackward, BCELossBackward
 from core.functions import mse, cross_entropy, binary_cross_entropy
-import numpy as np
 from core.tensor import Tensor
 
 class MSELoss:
     def forward(self, predictions: Tensor, targets: Tensor) -> Tensor:
-        err = mse(predictions.data, targets.data)
-        return Tensor(err)
+        out = Tensor(mse(predictions.data, targets.data))
+        out._grad_fn = MSELossBackward(predictions, targets)
+        return out
 
     def __call__(self, predictions: Tensor, targets: Tensor) -> Tensor:
         return self.forward(predictions, targets)
 
 class CrossEntropyLoss:
     def forward(self, logits: Tensor, targets: Tensor) -> Tensor:
-        res = cross_entropy(logits.data, targets.data)
-        return Tensor(res)
+        out = Tensor(cross_entropy(logits.data, targets.data))
+        out._grad_fn = CrossEntropyBackward(logits, targets)
+
+        return out
 
     def __call__(self, logits: Tensor, targets: Tensor) -> Tensor:
         return self.forward(logits, targets)
 
 class BinaryCrossEntropyLoss:
     def forward(self, predictions: Tensor, targets: Tensor) -> Tensor:
-        res = binary_cross_entropy(predictions.data, targets.data)
-        return Tensor(res)
+        out = Tensor(binary_cross_entropy(predictions.data, targets.data))
+        out._grad_fn = BCELossBackward(predictions, targets)
+
+        return out
 
     def __call__(self, predictions: Tensor, targets: Tensor) -> Tensor:
         return self.forward(predictions, targets)

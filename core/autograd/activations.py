fix: rebuilds graph nodes during backprop

core/autograd/activations.py
@@ -1,3 +1,4 @@
+import numpy as np
 from core.functions import sigmoid, tanh, softmax
 from core.tensor import Tensor
 from typing import override
@@ -8,48 +9,49 @@ class ReLUBackward(Function):
 
     @override
     def apply(self, grad_output:Tensor) -> tuple[Tensor, ...]:
-        t = self.saved_tensors[0]
+        t, = self.saved_tensors
         local_grad = (t > 0)
 
-        return grad_output * local_grad,
+        return Tensor(grad_output.data * local_grad.data),
 
 class SigmoidBackward(Function):
     """Gradient computation for Sigmoid activation."""
 
     @override
     def apply(self, grad_output:Tensor) -> tuple[Tensor, ...]:
-        t = self.saved_tensors[0]
+        t, = self.saved_tensors
         s = sigmoid(t.data)
         local_grad = s*(1-s)
 
-        return grad_output * local_grad,
+        return Tensor(grad_output.data * local_grad),
 
 class TanhBackward(Function):
     """Gradient computation for Tanh activation."""
 
     @override
     def apply(self, grad_output:Tensor) -> tuple[Tensor, ...]:
-        t = self.saved_tensors[0]
+        t, = self.saved_tensors
         local_grad = (1-tanh(t.data)**2)
 
-        return grad_output * local_grad,
+        return Tensor(grad_output.data * local_grad),
 
 class GELUBackward(Function):
     """Gradient computation for GELU activation."""
 
     @override
     def apply(self, grad_output:Tensor) -> tuple[Tensor, ...]:
-        t = self.saved_tensors[0]
+        t, = self.saved_tensors
         s = sigmoid(t.data * 1.702)
-        local_grad: Tensor = s * (1 + 1.702 * t * (1-s))
+        local_grad = s * (1 + 1.702 * t.data * (1-s))
 
-        return grad_output * local_grad,
+        return Tensor(grad_output.data * local_grad),
 
 class SoftmaxBackward(Function):
     """Gradient computation for Softmax activation."""
 
     @override
     def apply(self, grad_output:Tensor) -> tuple[Tensor, ...]:
-        t = softmax(self.saved_tensors[0].data)
-        dot = (grad_output*t).sum(axis=-1, keepdims=True)
-        return t * (grad_output - dot),
+        t, = self.saved_tensors
+        s = softmax(t.data)
+        dot = np.sum(grad_output.data*s, axis=-1, keepdims=True)
+        return Tensor(t * (grad_output.data - dot)),

core/autograd/arithmetic.py
@@ -46,10 +46,10 @@ class DivBackward(Function):
         grad_a = grad_b = np.array([])
 
         if a.requires_grad:
-            grad_a = grad_output * (1/b.data)
+            grad_a = grad_output.data * (1/b.data)
 
         if b.requires_grad:
-            grad_b = grad_output * -a/(b**2)
+            grad_b = grad_output.data * -a.data/(b.data**2)
 
         return Tensor(grad_a), Tensor(grad_b)
 
@@ -77,7 +77,9 @@ class SumBackward(Function):
 
     @override
     def apply(self, grad_output: Tensor) -> tuple[Tensor, ...]:
-        return grad_output,
+        t, = self.saved_tensors
+        res = Tensor(np.broadcast_to(grad_output.data, t.shape))
+        return res,
 
 class ReshapeBackward(Function):
     """Gradient computation for reshape operation."""
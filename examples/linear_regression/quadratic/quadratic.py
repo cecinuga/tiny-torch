143c8591c49851f5a4b817e708b41f831281a1cd
cecinuga <cecinuga.tdb@gmail.com>
2026-07-19 20:32:49 +0200

    achive quadratic

--- examples/linear_regression/quadratic/quadratic.py
+++ examples/linear_regression/quadratic/quadratic.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Configuration
-EPOCHS = 100
+EPOCHS = 25
 TEST_STEP = 5
 DATASET_SIZE = 50
 NOISY = 8
@@ -15,32 +15,31 @@ NOISY = 8
 # To estimate function
 def f(x: np.ndarray):
     """The function we want to estimate"""
-    return x**2
+    return x**2 + x*2 + 2
 
 
 # Dataset
 train_domain = np.linspace(-5, 5, DATASET_SIZE).reshape(-1, 1)
 test_domain = np.linspace(-10, 10, DATASET_SIZE).reshape(-1, 1)
-y_train = f(train_domain)
-y_test = f(test_domain)
-train_y = Tensor(y_train + np.random.uniform(-1, 1, y_train.shape)*NOISY)
-train_x = Tensor(train_domain)
-test_y = Tensor(y_test + np.random.uniform(-1, 1, y_test.shape)*NOISY)
-test_x = Tensor(test_domain)
-
+f_train = f(train_domain)
+f_test = f(test_domain)
+X_train = Tensor(np.hstack([train_domain, train_domain**2]))
+X_test = Tensor(np.hstack([test_domain, test_domain**2]))
+train_y = Tensor(f_train + np.random.uniform(-1, 1, f_train.shape)*NOISY)
+test_y = Tensor(f_test + np.random.uniform(-1, 1, f_test.shape)*NOISY)
 
 # Model architecture
 model = Sequential(
-    Linear(1, 1),
+    Linear(2, 1),
 )
 loss = MSELoss()
-optimizer = SGD(model.parameters, 1e-2)
+optimizer = SGD(model.parameters, 1e-3)
 
 
 # Save graph of models
-#model.save_graph("./examples/linear_regression/arch/architecture.png", arch=True)
-#model.save_graph("./examples/linear_regression/arch/forward.png", arch=False, forward=True)
-#model.save_graph("./examples/linear_regression/arch/backward.png", arch=False, backward=True)
+model.save_graph("./examples/linear_regression/quadratic/arch/architecture.png", arch=True)
+model.save_graph("./examples/linear_regression/quadratic/arch/forward.png", arch=False, forward=True)
+model.save_graph("./examples/linear_regression/quadratic/arch/backward.png", arch=False, backward=True)
 
 
 # Train loop
@@ -48,12 +47,13 @@ test_losses: list[np.ndarray] = []
 train_losses: list[np.ndarray] = []
 
 for i in range(EPOCHS):
-    pred = model(train_x)
+    pred = model(X_train)
     train_loss = loss(pred, train_y)
     train_losses.append(train_loss.data)
+
     if i%TEST_STEP == 0 or i == EPOCHS-1:
         model.eval()
-        test_pred = model(test_x)
+        test_pred = model(X_test)
         test_loss = loss(test_pred, test_y)
         test_losses.append(test_loss.data)
         model.train()
@@ -62,8 +62,11 @@ for i in range(EPOCHS):
     optimizer.step()
     optimizer.zero_grad()
 
+
 # Eval
-out = model(test_x)
+model.eval()
+out = model(X_test)
+
 
 # Plot in one window, divided into sections
 fig, (ax_loss, ax_result) = plt.subplots(1, 2, figsize=(12, 5))
@@ -76,10 +79,11 @@ ax_loss.set_xlabel("epoch")
 ax_loss.set_ylabel("loss")
 ax_loss.legend()
 
+
 # Result section (the regression)
-ax_result.plot(test_x.data, test_y.data, 'r.', label="target")
-ax_result.plot(test_x.data, out.data, label="prediction")
-ax_result.plot(test_x.data, y_test, label='to estimate')
+ax_result.plot(test_domain, test_y.data, 'r.', label="target")
+ax_result.plot(test_domain, out.data, label="prediction")
+ax_result.plot(test_domain, f_test, label='to estimate')
 ax_result.set_title("Regression")
 ax_result.set_xlabel("x")
 ax_result.set_ylabel("y")

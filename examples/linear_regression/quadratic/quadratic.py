8983c54383e8b69eff5f7aa0b3cc91200dbff9eb
cecinuga <cecinuga.tdb@gmail.com>
2026-07-23 22:43:36 +0200

    integrated Trainer inside quadratic & cubic

--- examples/linear_regression/quadratic/quadratic.py
+++ examples/linear_regression/quadratic/quadratic.py
@@ -1,15 +1,19 @@
+from core.dataset import DataLoader, TensorDataset
 from core.optimizer import SGD
 from core.tensor import Tensor
 from core.losses import MSELoss
+from core.training import Trainer, CosineSchedule
 from core.layers import Sequential, Linear
 import numpy as np
 import matplotlib.pyplot as plt
 
 # Configuration
 EPOCHS = 25
-TEST_STEP = 5
-DATASET_SIZE = 50
+EVAL_STEP = 5
+DATASET_SIZE = 100
 NOISY = 8
+MAX_LR = 1e-3
+MIN_LR = 1e-6
 
 
 # To estimate function
@@ -23,58 +27,55 @@ train_domain = np.linspace(-5, 5, DATASET_SIZE).reshape(-1, 1)
 test_domain = np.linspace(-10, 10, DATASET_SIZE).reshape(-1, 1)
 f_train = f(train_domain)
 f_test = f(test_domain)
-X_train = Tensor(np.hstack([train_domain, train_domain**2]))
-X_test = Tensor(np.hstack([test_domain, test_domain**2]))
+train_x = Tensor(np.hstack([train_domain, train_domain**2]))
+test_x = Tensor(np.hstack([test_domain, test_domain**2]))
 train_y = Tensor(f_train + np.random.uniform(-1, 1, f_train.shape)*NOISY)
 test_y = Tensor(f_test + np.random.uniform(-1, 1, f_test.shape)*NOISY)
 
+train_dataset = TensorDataset(train_x, train_y)
+train_dataloader = DataLoader(train_dataset, DATASET_SIZE)
+
+test_dataset = TensorDataset(test_x, test_y)
+test_dataloader = DataLoader(test_dataset, DATASET_SIZE)
 
 # Model architecture
 model = Sequential(
     Linear(2, 1),
 )
 loss = MSELoss()
-optimizer = SGD(model.parameters, 1e-3)
-
+optimizer = SGD(model.parameters, MAX_LR)
+scheduler = CosineSchedule(MAX_LR, MIN_LR, EPOCHS)
+trainer = Trainer(
+    model,
+    loss,
+    optimizer,
+    scheduler
+)
 
 # Save graph of models
-model.save_graph("./examples/linear_regression/quadratic/arch/architecture.png", arch=True)
-model.save_graph("./examples/linear_regression/quadratic/arch/forward.png", arch=False, forward=True)
-model.save_graph("./examples/linear_regression/quadratic/arch/backward.png", arch=False, backward=True)
+#model.save_graph("./examples/linear_regression/quadratic/arch/architecture.png", arch=True)
+#model.save_graph("./examples/linear_regression/quadratic/arch/forward.png", arch=False, forward=True)
+#model.save_graph("./examples/linear_regression/quadratic/arch/backward.png", arch=False, backward=True)
 
 
 # Train loop
-test_losses: list[np.ndarray] = []
-train_losses: list[np.ndarray] = []
+for i, epoch in enumerate(range(EPOCHS)):
+    _ = trainer.train_epoch(train_dataloader, 1)
 
-for i in range(EPOCHS):
-    pred = model(X_train)
-    train_loss = loss(pred, train_y)
-    train_losses.append(train_loss.data)
-
-    if i%TEST_STEP == 0 or i == EPOCHS-1:
-        model.eval()
-        test_pred = model(X_test)
-        test_loss = loss(test_pred, test_y)
-        test_losses.append(test_loss.data)
-        model.train()
-
-    train_loss.backward()
-    optimizer.step()
-    optimizer.zero_grad()
+    if i % EVAL_STEP == 0 :
+        _ = trainer.eval(test_dataloader)
 
 
 # Eval
-model.eval()
-out = model(X_test)
+out = model(test_x)
 
 
 # Plot in one window, divided into sections
 fig, (ax_loss, ax_result) = plt.subplots(1, 2, figsize=(12, 5))
 
 # Loss section
-ax_loss.plot(list(range(EPOCHS)), train_losses, label="train")
-ax_loss.plot(list(range(0, EPOCHS+1, TEST_STEP)), test_losses, label="test")
+ax_loss.plot(list(range(EPOCHS)), trainer.train_loss, label="train")
+ax_loss.plot(list(range(0, EPOCHS, EVAL_STEP)), trainer.eval_loss, label="test")
 ax_loss.set_title("Loss")
 ax_loss.set_xlabel("epoch")
 ax_loss.set_ylabel("loss")

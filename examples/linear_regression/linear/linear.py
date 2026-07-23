a4d31fadc7a980111a2d05403197b3b4277dd703
cecinuga <cecinuga.tdb@gmail.com>
2026-07-23 22:13:05 +0200

    refactoring with Trainer

--- examples/linear_regression/linear/linear.py
+++ examples/linear_regression/linear/linear.py
@@ -1,14 +1,16 @@
+from core.dataset import DataLoader, TensorDataset
 from core.optimizer import SGD
 from core.tensor import Tensor
 from core.losses import MSELoss
+from core.training import Trainer
 from core.layers import Sequential, Linear
 import numpy as np
 import matplotlib.pyplot as plt
 
 # Configuration
 EPOCHS = 150
-TEST_STEP = 5
-DATASET_SIZE = 50
+EVAL_STEP = 5
+DATASET_SIZE = 100
 NOISY = 8
 
 
@@ -28,13 +30,11 @@ train_x = Tensor(train_domain)
 test_y = Tensor(y_test + np.random.uniform(-1, 1, y_test.shape)*NOISY)
 test_x = Tensor(test_domain)
 
+train_dataset = TensorDataset(train_x, train_y)
+train_dataloader = DataLoader(train_dataset, DATASET_SIZE)
 
-# Testing with closed form
-# Design matrix [x, 1] so lstsq fits both slope (m) and intercept (c)
-A = np.hstack([test_x.data, np.ones_like(test_x.data)])
-m, c = np.linalg.lstsq(A, test_y.data.ravel(), rcond=None)[0]
-closed_form = test_x.data*m + c
-
+test_dataset = TensorDataset(test_x, test_y)
+test_dataloader = DataLoader(test_dataset, DATASET_SIZE)
 
 # Model architecture
 model = Sequential(
@@ -42,7 +42,11 @@ model = Sequential(
 )
 loss = MSELoss()
 optimizer = SGD(model.parameters, 1e-2)
-
+trainer = Trainer(
+    model,
+    loss,
+    optimizer
+)
 
 # Save graph of models
 #model.save_graph("./examples/linear_regression/simple/arch/architecture.png", arch=True)
@@ -51,33 +55,31 @@ optimizer = SGD(model.parameters, 1e-2)
 
 
 # Train loop
-test_losses: list[np.ndarray] = []
-train_losses: list[np.ndarray] = []
+for i, epoch in enumerate(range(EPOCHS)):
+    _ = trainer.train_epoch(train_dataloader, 1)
 
-for i in range(EPOCHS):
-    pred = model(train_x)
-    train_loss = loss(pred, train_y)
-    train_losses.append(train_loss.data)
-    if i%TEST_STEP == 0 or i == EPOCHS-1:
-        model.eval()
-        test_pred = model(test_x)
-        test_loss = loss(test_pred, test_y)
-        test_losses.append(test_loss.data)
-        model.train()
+    if i % EVAL_STEP or i == EVAL_STEP-1 :
+        _ = trainer.eval(test_dataloader)
 
-    train_loss.backward()
-    optimizer.step()
-    optimizer.zero_grad()
 
 # Eval
 out = model(test_x)
+
+
+# Testing with closed form
+# Design matrix [x, 1] so lstsq fits both slope (m) and intercept (c)
+A = np.hstack([test_x.data, np.ones_like(test_x.data)])
+m, c = np.linalg.lstsq(A, test_y.data.ravel(), rcond=None)[0]
+closed_form = test_x.data*m + c
+
+
 
 # Plot in one window, divided into sections
 fig, (ax_loss, ax_result) = plt.subplots(1, 2, figsize=(12, 5))
 
 # Loss section
-ax_loss.plot(list(range(EPOCHS)), train_losses, label="train")
-ax_loss.plot(list(range(0, EPOCHS+1, TEST_STEP)), test_losses, label="test")
+ax_loss.plot(list(range(EPOCHS)), trainer.history['train_losses'], label="train")
+ax_loss.plot(list(range(0, EPOCHS+1, EVAL_STEP)), trainer.history['eval_losses'], label="test")
 ax_loss.set_title("Loss")
 ax_loss.set_xlabel("epoch")
 ax_loss.set_ylabel("loss")
